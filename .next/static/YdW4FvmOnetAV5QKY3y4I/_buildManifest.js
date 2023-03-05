79e7e0b34a1f4b02cdd784be10986f3e849f200b
Samomen <godsamomen@protonmail.com>
2023-03-06 01:26:23 +0300

    Delete .next directory

--- .next/static/YdW4FvmOnetAV5QKY3y4I/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/679-e02e10046f0f19a6.js","static/css/86b5a40e9aee9e8f.css","static/chunks/pages/index-f237e54f94d11c24.js"],"/_error":["static/chunks/pages/_error-8353112a01355ec2.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
