0ac4c5a349443abe7a2c3becfb1ae4db997bac35
Samomen <godsamomen@protonmail.com>
2023-03-05 16:55:09 +0300

    a

--- components/DropDown.tsx
+++ components/DropDown.tsx
@@ -10,21 +10,21 @@ function classNames(...classes: string[]) {
   return classes.filter(Boolean).join(" ");
 }
 
-export type VibeType = "Professional" | "Casual" | "Funny";
+export type LitType   = "Genel" | "Şiir" | "Özet" | "Hikaye" | "Deneme";
 
 interface DropDownProps {
-  vibe: VibeType;
-  setVibe: (vibe: VibeType) => void;
+  lit: LitType;
+  setLit: (lit: LitType) => void;
 }
 
-let vibes: VibeType[] = ["Professional", "Casual", "Funny"];
+let lits: LitType[] = ["Genel", "Şiir", "Özet", "Hikaye", "Deneme"];
 
-export default function DropDown({ vibe, setVibe }: DropDownProps) {
+export default function DropDown({ lit, setLit }: DropDownProps) {
   return (
     <Menu as="div" className="relative block text-left w-full">
       <div>
         <Menu.Button className="inline-flex w-full justify-between items-center rounded-md border border-gray-300 bg-white px-4 py-2 text-gray-700 shadow-sm hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-black">
-          {vibe}
+          {lit}
           <ChevronUpIcon
             className="-mr-1 ml-2 h-5 w-5 ui-open:hidden"
             aria-hidden="true"
@@ -47,22 +47,22 @@ export default function DropDown({ vibe, setVibe }: DropDownProps) {
       >
         <Menu.Items
           className="absolute left-0 z-10 mt-2 w-full origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-          key={vibe}
+          key={lit}
         >
-          <div className="">
-            {vibes.map((vibeItem) => (
-              <Menu.Item key={vibeItem}>
+          <div className="py-1">
+            {lits.map((litItem) => (
+              <Menu.Item key={litItem}>
                 {({ active }) => (
                   <button
-                    onClick={() => setVibe(vibeItem)}
+                    onClick={() => setLit(litItem)}
                     className={classNames(
                       active ? "bg-gray-100 text-gray-900" : "text-gray-700",
-                      vibe === vibeItem ? "bg-gray-200" : "",
+                      lit === litItem ? "bg-gray-200" : "",
                       "px-4 py-2 text-sm w-full text-left flex items-center space-x-2 justify-between"
                     )}
                   >
-                    <span>{vibeItem}</span>
-                    {vibe === vibeItem ? (
+                    <span>{litItem}</span>
+                    {lit === litItem ? (
                       <CheckIcon className="w-4 h-4 text-bold" />
                     ) : null}
                   </button>
